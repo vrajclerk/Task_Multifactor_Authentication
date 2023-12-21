f93ac48313cb70e3e787f94d3b8907aa23b0f742
Vraj Clerk <vrajclerk04@gmail.com>
2023-12-22 01:12:35 +0530

    21-dec last changes

--- send_mail.js
+++ send_mail.js
@@ -5,27 +5,24 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: 'tester31190@gmail.com', 
+    
       pass: 'ehjo azwp qojm uiuc', 
     },
   });
 
   const mailOptions = {
     from: 'tester31190@gmail.com', 
-    to: userEmail,
+    to: email,
     subject: 'OTP Verification',
     text: `Your OTP is: ${otp}`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error)
-      return res.status(500).send("error occur");
+    if (error) {     
+      return res.status(500).send("error occur");;
     }
-    ob.otp = otp
-    ob.email = userEmail
-    ob.password = userPassword
-    res.status(200).render('verify_otp',{otp1:"otp send successfully ",email:userEmail,redirect:"/signup/verify_otp"});
     
   });
+
 }
-  module.exports={send_mail};
+module.exports={send_mail};
